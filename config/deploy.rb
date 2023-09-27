3dff059e47c239b0a6d3a0d1de22ada9fea967a6
Reni-amorim <reni.425@gmail.com>
2023-09-27 01:55:16 +0000

    set rvm trust project

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,14 @@ set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
+#set deploy config to trust project https://rvm.io/deployment/capistrano#environment
+
+namespace :rvm do
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
+  end
+end
+
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
@@ -33,6 +41,8 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
