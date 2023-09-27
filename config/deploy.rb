769995cd908aa463faa7a9113890551664dd4f51
Reni-amorim <reni.425@gmail.com>
2023-09-27 05:14:56 +0000

    set ip server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server "54.233.152.209", :roles => [:app, :web, :db], :primary => true
+server "15.229.28.1", :roles => [:app, :web, :db], :primary => true
 
 set :repo_url,        'git@github.com:Reni-amorim/blog.git'
 set :application,     'blog'
